4a1fc8fb77d417527f5c2ba2b8a2eb80d6f346a2
test <test@localhost.localdomain>
2024-02-08 11:56:43 -0500

    init terraform X kubernetes

---
+++ kubernetes.tf
@@ -0,0 +1,107 @@
+### TERRAFORM KUBERNETES PROVIDER ###
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "host" {
+  type = string
+}
+
+variable "client_certificate" {
+  type = string
+}
+
+variable "client_key" {
+  type = string
+}
+
+variable "cluster_ca_certificate" {
+  type = string
+}
+
+provider "kubernetes" {
+  host = var.host
+
+  client_certificate     = base64decode(var.client_certificate)
+  client_key             = base64decode(var.client_key)
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+}
+
+### CONFIGURATION TO SCHEDULE AN NGINX DEPLOYMENT ###
+### WITH 2 REPLICAS ON KUBERNETES CLUSTER ###
+### INTERNALLY EXPOSING EXPOSING PORT 80 (HTTP) ###
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 8
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          name  = "example"
+          image = "nginx:1.7.8"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+### IF CLUSTER IS HOSTED LOCALLY ON KIND ###
+### EXPOSE NGINX INSTANCE VIA NodePort TO ACCESS INSTANCE ###
+### THIS EXPOSES SERVICE ON EACH NODE'S IP AT A STATIC PORT ###
+### ALLOWING YOU TO ACCESS THE SERVICE FROM OUTSIDE THE CLUSTER AT ###
+### <NodeIP>:<NodePort> ###
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
